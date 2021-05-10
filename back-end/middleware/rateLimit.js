6b7a78ee177402fa4016a63f448445cd961302d9
emi <bolo.emilie@gmail.com>
2021-05-10 10:44:31 +0200

    cretae middleware/ratelimit.js

---
+++ back-end/middleware/rateLimit.js
@@ -0,0 +1,16 @@
+const rateLimit = require("express-rate-limit");
+
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+module.exports = rateLimit({ 
+
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+
+  windowMs: 60 * 60 * 1000, // 1 hour window
+  max: 5, // start blocking after 5 requests
+  message:
+    "Too many accounts created from this IP, please try again after an hour"
+
+});
