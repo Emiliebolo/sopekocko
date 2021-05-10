747e6725aa753f1d98f3a0f06414204d71084605
emi <bolo.emilie@gmail.com>
2021-05-10 10:42:07 +0200

    create models/user.js

---
+++ back-end/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+/* mongoose-unique-validator est un plugin qui ajoute une validation de pré-sauvegarde pour les champs uniques dans un schéma Mongoose.
+Cela rend la gestion des erreurs beaucoup plus facile, car vous obtiendrez une erreur de validation Mongoose lorsque vous tenterez de violer une contrainte unique , plutôt qu'une erreur E11000 de MongoDB. */
+
+var uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = mongoose.Schema({
+  username : {  type : String, required: true, unique: true } ,
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+userSchema.plugin(uniqueValidator);
+module.exports = mongoose.model('User', userSchema);
